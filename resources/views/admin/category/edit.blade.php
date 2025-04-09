06e171fbb920d9912b2c483e7e5015989996555b
Pothe <keoutpothe2014@gmail.com>
2025-04-09 07:38:59 +0700

    Update edit.blade.php

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -1,12 +1,12 @@
 @extends('Layouts.app')
-@section('title','create new category')
+@section('title','edit the category')
     
 @section('content')
 <div class="container my-5">
     <div class="row">
       <div class="d-flex justify-content-between mb-2">
-        <h3>Create Category</h3>
-        <a class="btn btn-success" href="index.html" role="button">Back</a>
+        <h3>update Category</h3>
+        <a class="btn btn-success" href="{{ route('admin.cat') }}" role="button"><i class="bi bi-arrow-left-circle"></i></a>
       </div>
       <!-- Blog entries-->
       <div class="col-lg-12">
@@ -18,7 +18,7 @@
               <label for="tag" class="form-label">Category</label>
               <input type="text" class="form-control" id="name" name="name" value="{{  $single_category->name }}"/>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">update</button>
           </form>
         </div>
       </div>
