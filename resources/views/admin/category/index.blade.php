54672bfadd0ed8461d8d50c9a30a01ff7f36b8cb
Pothe <keoutpothe2014@gmail.com>
2025-04-09 08:15:50 +0700

    Update index.blade.php

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -5,6 +5,32 @@
 @section('content')
 
 <div class="container my-5">
+  {{-- @if (session('success'))
+       <div class="alert alert-primary alert-dismissible fade show" role="alert">
+      {{ session('success') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif --}}
+
+@if(session('success'))
+<script>
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    Toast.fire({
+        icon: "success",
+        title: "{{ session('success') }}"
+    });
+</script>
+@endif
     <div class="row">
       <div class="d-flex justify-content-between mb-2">
         <h3>Category List</h3>
@@ -40,13 +66,16 @@
                     href="{{ route('admin.cat.edit',['id' => $item->id ]) }}"
                     role="button"
                     ><i class="bi bi-pencil-square"></i></a
-                  > ||
-                  <a
-                  class="btn btn-danger btn-sm"
-                  href="{{ route('admin.cat.edit',['id' => $item->id ]) }}"
-                  role="button"
-                  ><i class="bi bi-trash3-fill \"></i></a
-                >
+                  > 
+                  
+                  <form action="{{ route('admin.cat.destroy',$item->id) }}" method="post">
+                    @method('delete')
+                    @csrf
+
+                    <button type="submit" type="button" onclick="return confirm('Are you sure you want to delete this user?')" class="btn-danger"><i class="bi bi-trash3-fill"></i> </button>
+                  </form>
+                  
+                
                 </td>
               </tr>
               @endforeach
