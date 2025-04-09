0a05adacb7eb46176d23a286e2fdc2f0feacd760
Pothe <keoutpothe2014@gmail.com>
2025-04-09 11:11:08 +0700

    Create create.blade.php

---
+++ resources/views/admin/tags/create.blade.php
@@ -0,0 +1,34 @@
+@extends('Layouts.app')
+@section('title','create new category')
+    
+@section('content')
+<div class="container my-5">
+  @include('components.error')
+    <div class="row">
+      <div class="d-flex justify-content-between mb-2">
+        <h3>Create Category</h3>
+        <a class="btn btn-success" href="" role="button"><i class="bi bi-arrow-left-circle"></i></a>
+      </div>
+      <!-- Blog entries-->
+      <div class="col-lg-12">
+        <div class="card p-3">
+          <form method="POST" action="{{ route('admin.cat.store')}}">
+            @csrf
+            <div class="mb-3">
+              <label for="tag" class="form-label">Category</label>
+              <input type="text" class="form-control @error('name') is-invalid @enderror" id="tag" name="name" />
+             @error('name')
+            <div class="invalid-feedback">
+             {{ $message }}
+            </div>
+                 
+             @enderror
+            </div>
+            <button type="submit" class="btn btn-primary">save</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
