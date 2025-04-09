3294db016d837661c7e2f5e9e21920cffc2438e1
Pothe <keoutpothe2014@gmail.com>
2025-04-09 07:39:12 +0700

    Update create.blade.php

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
       <div class="d-flex justify-content-between mb-2">
         <h3>Create Category</h3>
-        <a class="btn btn-success" href="{{ route('admin.cat') }}" role="button">Back</a>
+        <a class="btn btn-success" href="{{ route('admin.cat') }}" role="button"><i class="bi bi-arrow-left-circle"></i></a>
       </div>
       <!-- Blog entries-->
       <div class="col-lg-12">
@@ -15,9 +15,15 @@
             @csrf
             <div class="mb-3">
               <label for="tag" class="form-label">Category</label>
-              <input type="text" class="form-control" id="tag" name="name" />
+              <input type="text" class="form-control @error('name') is-invalid @enderror" id="tag" name="name" />
+             @error('name')
+            <div class="invalid-feedback">
+             {{ $message }}
             </div>
-            <button type="submit" class="btn btn-primary">save <i class="bi bi-floppy2"></i></button>
+                 
+             @enderror
+            </div>
+            <button type="submit" class="btn btn-primary">save</button>
           </form>
         </div>
       </div>
