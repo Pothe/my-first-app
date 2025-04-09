4d2ff8f87f459aabeb7e0b759dafd3d734758d98
Pothe <keoutpothe2014@gmail.com>
2025-04-09 07:39:05 +0700

    Update app.blade.php

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -11,6 +11,7 @@
     <title>@yield('title')</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
       rel="stylesheet"
