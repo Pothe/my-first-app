ee406772916913f2809cec9042210034724f21a8
Pothe <keoutpothe2014@gmail.com>
2025-04-09 10:41:59 +0700

    Create error.blade.php

---
+++ resources/views/components/error.blade.php
@@ -0,0 +1,21 @@
+@if(session('error'))
+
+<script>
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    Toast.fire({
+        icon: "success",
+        title: "{{ session('error') }}"
+    });
+</script>
+
+@endif
