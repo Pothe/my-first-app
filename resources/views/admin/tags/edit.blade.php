483b25e2bc5151a57626f50ceda4fc61507ac854
Pothe <keoutpothe2014@gmail.com>
2025-04-16 14:32:54 +0700

    Update edit.blade.php

--- resources/views/admin/tags/edit.blade.php
+++ resources/views/admin/tags/edit.blade.php
@@ -6,17 +6,16 @@
     <div class="row">
       <div class="d-flex justify-content-between mb-2">
         <h3>update Category</h3>
-        <a class="btn btn-success" href="{{ route('admin.cat') }}" role="button"><i class="bi bi-arrow-left-circle"></i></a>
+        <a class="btn btn-success" href="#" role="button"><i class="bi bi-arrow-left-circle"></i></a>
       </div>
       <!-- Blog entries-->
       <div class="col-lg-12">
         <div class="card p-3">
-          <form method="POST"​ action="{{ route('admin.cat.update',$single_category->id) }}">  
-            @csrf
-            @method('PUT')  
+          <form method="POST"​ action="#">  
+           
             <div class="mb-3">
               <label for="tag" class="form-label">Category</label>
-              <input type="text" class="form-control" id="name" name="name" value="{{  $single_category->name }}"/>
+              <input type="text" class="form-control" id="name" name="name" value=""/>
             </div>
             <button type="submit" class="btn btn-primary">update</button>
           </form>
