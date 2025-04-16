1728c26c1a2e4da32f225f862a11aa9752ed8576
Pothe <keoutpothe2014@gmail.com>
2025-04-16 09:00:00 +0700

    Update index.blade.php

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -1,10 +1,63 @@
 @extends('Layouts.app')
-@section('title','create new category')
+@section('title', "all categories")
     
-@section('content')
-<div class="container my-5">
- <h1>tag</h1>
-   
-  </div>
-@endsection
 
+@section('content')
+
+<div class="container my-5">
+
+  @include('components.alert')
+  @include('components.error')
+
+    <div class="row">
+      <div class="d-flex justify-content-between mb-2">
+        <h3>Tags List</h3>
+        <a class="btn btn-success" href="{{ route('admin.tag.create') }}" role="button"
+          ><i class="bi bi-plus-circle-fill"></i></a
+        >
+      </div>
+      <!-- Blog entries-->
+      <div class="col-lg-12">
+        <div class="card p-3">
+          <table
+            id="datatable"
+            class="table table-striped"
+            style="width: 100%"
+          >
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Category</th>
+                <th style="width: 100px">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+
+       
+            
+                  {{-- <a
+                  class="btn btn-primary btn-sm"
+                  href="{{ route('admin.cat.edit',['id' => $item->id ]) }}"
+                  role="button"
+                  ><i class="bi bi-pencil-square"></i></a> --}}
+              
+                  
+                {{-- <form action="{{ route('admin.cat.destroy',$item->id) }}" method="post">
+                  @method('delete')
+                  @csrf
+
+                  <button type="submit" type="button" onclick="return confirm('Are you sure you want to delete this user?')" class="btn-danger"><i class="bi bi-trash3-fill"></i> </button>
+                </form>    --}}
+               
+              </tr>
+           
+             
+              
+            </tbody>
+            
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  @endsection
