9beae6045ce07c04dad8f5684b86ab67e98e7004
Pothe <keoutpothe2014@gmail.com>
2025-04-16 08:59:52 +0700

    Update create.blade.php

--- resources/views/admin/tags/create.blade.php
+++ resources/views/admin/tags/create.blade.php
@@ -1,21 +1,21 @@
 @extends('Layouts.app')
-@section('title','create new category')
+@section('title','create new tag')
     
 @section('content')
 <div class="container my-5">
   @include('components.error')
     <div class="row">
       <div class="d-flex justify-content-between mb-2">
-        <h3>Create Category</h3>
-        <a class="btn btn-success" href="" role="button"><i class="bi bi-arrow-left-circle"></i></a>
+        <h3>Create Tag</h3>
+        <a class="btn btn-success" href="{{ route('admin.tags') }}" role="button"><i class="bi bi-arrow-left-circle"></i></a>
       </div>
       <!-- Blog entries-->
       <div class="col-lg-12">
         <div class="card p-3">
-          <form method="POST" action="{{ route('admin.cat.store')}}">
+          <form method="POST" action="{{ route('admin.tag.store')}}">
             @csrf
             <div class="mb-3">
-              <label for="tag" class="form-label">Category</label>
+              <label for="tag" class="form-label">Tag</label>
               <input type="text" class="form-control @error('name') is-invalid @enderror" id="tag" name="name" />
              @error('name')
             <div class="invalid-feedback">
