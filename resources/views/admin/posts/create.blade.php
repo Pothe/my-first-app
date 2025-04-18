cf76e8a020b8b5e7277edf52e5ed2f2e73cb7273
Pothe <keoutpothe2014@gmail.com>
2025-04-18 10:20:44 +0700

    Update create.blade.php

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -18,19 +18,33 @@
               <label for="title" class="form-label">Title</label>
               <input
                 type="text"
-                class="form-control"
+                class="form-control @error("title")
+                   is-invalid
+                @enderror"
                 id="title"
                 name="title"
+                value="{{ old('title') }}"
               />
+              @error('title')
+              <span class=" is-invalid text-danger">{{ $message }}</span>
+                
+              @enderror
             </div>
             <div class="mb-3">
               <label for="content" class="form-label">Content</label>
               <textarea
-                class="form-control"
+                class="form-control @error('content')
+                  is-invalid
+                @enderror"
                 id="content"
                 name="content"
                 rows="5"
+              
               ></textarea>
+              @error('content')
+              <span class=" is-invalid text-danger">{{ $message }}</span>
+              @enderror
+              
             </div>
             <div class="mb-3">
               <label for="thumbnail" class="form-label"
